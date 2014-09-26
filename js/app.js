f3e21a653ace55aa3953eef2619f50aaedbf9068
Alfredo Miranda <alfredo@miranda.io>
2014-09-25 23:52:17 -0500

    Remove form element

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
-$(document).ready(function(){
+$(function(){
+  var secret;
+  function newGame() {
+    secretNumber();
+  }
+
+  function secretNumber() {
+    secret = Math.floor(Math.random() * 100 + 1);
+  }
+
   /*--- Display information modal box ---*/
   $(".what").click(function(){
     $(".overlay").fadeIn(1000);
   });
+
   /*--- Hide information modal box ---*/
   $("a.close").click(function(){
     $(".overlay").fadeOut(1000);
+  });
+
+  $("#userGuess").keydown(function(event) {
+    if(event.which == 13) {
+      var guess = $(this).val();
+      console.log('guess', guess);
+    }
   });
 });
